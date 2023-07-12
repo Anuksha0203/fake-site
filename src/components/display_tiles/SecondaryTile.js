dfbddd7519a0424960eab52b2f47b72fc6c45937
Ayesha Samarawickrama <ayeshasamarawickrama@Ayeshas-MacBook-Air.local>
2023-07-12 20:20:29 +0530

    Made a link to products displayed on secondary tile

--- src/components/display_tiles/SecondaryTile.js
+++ src/components/display_tiles/SecondaryTile.js
@@ -10,17 +10,21 @@ function SecondaryTile() {
       <div className="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3 justify-content-center">
         <div className="bg-light mt-3 mt-md-0 me-md-3 pt-3 px-3 pt-md-5 px-lg-5 text-center overflow-hidden w-md-50">
           <div className="my-3 py-3">
-            <h2 className="display-5">Primary Product</h2>
-            <p className="lead">Product description</p>
+            <h2 className="display-5">Lavender</h2>
+            <p className="lead">Lavender, Bitter Orange</p>
           </div>
-          <img src={lavender} width='100%' />
+          <a href="/shop/g660vnhISO8H5QDJPSQ1" target="_blank" rel="noreferrer">
+            <img src={lavender} width='100%' />
+          </a>
         </div>
         <div className="text-bg-dark mt-3 mt-md-0 me-md-3 pt-3 px-3 pt-md-5 px-lg-5 text-center overflow-hidden w-md-50">
           <div className="my-3 py-3">
-            <h2 className="display-5 text-white">Secondary Product</h2>
-            <p className="lead">Product description</p>
+            <h2 className="display-5 text-white">Sage</h2>
+            <p className="lead">Sage, Citrus</p>
           </div>
-          <img src={sage} width='100%' />
+          <a href="/shop/0ANbLeDsnXG6v3zFGyu8" target="_blank" rel="noreferrer">
+            <img src={sage} width='100%' />
+          </a>
         </div>
       </div>
     </>
