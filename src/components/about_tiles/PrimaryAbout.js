6a43941e755515bee0ff15881255111f61a5379d
Anuksha0203 <65045527+Anuksha0203@users.noreply.github.com>
2023-07-17 18:50:11 +0100

    Update PrimaryAbout.js colour

--- src/components/about_tiles/PrimaryAbout.js
+++ src/components/about_tiles/PrimaryAbout.js
@@ -6,7 +6,7 @@ function PrimaryAbout(props) {
   return (
     <>
 
-      <div className="row text-bg-light p-3 p-md-5 m-md-3 overflow-hidden text-center">
+      <div className="row text-bg-beige p-3 p-md-5 m-md-3 overflow-hidden text-center">
 
         <div className="col-lg-7 p-lg-5 mx-auto my-5" > {/* my-3 py-3 mx-auto*/}
           <h1 className="display-4 fw-normal">About Us</h1>
