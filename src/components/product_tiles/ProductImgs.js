ff0fedeff8f74619d2aae1d1447bf8d5fd803f1c
Anuksha0203 <65045527+Anuksha0203@users.noreply.github.com>
2023-07-19 20:31:46 +0100

    Update ProductImgs.js

--- src/components/product_tiles/ProductImgs.js
+++ src/components/product_tiles/ProductImgs.js
@@ -15,28 +15,29 @@ function ProductImgs(props) {
         </button>
       </div>
       <div style={{float: 'left'}}>
-        <img
-          ref={imageRef}
-          src={ props.images[currentIndex] }
-          width="300"
-          style={{margin: '2px'}}
-          alt=""
-          role="button"
-        />
-        <br/>
-
-        {props.images.map((src, index) => (
+        <div>
           <img
-            src={ src }
-            onClick={() => changeImage(src, index)}
-            width="97.3"
-            height ="80"
-            key={ index }
+            ref={imageRef}
+            src={ props.images[currentIndex] }
+            width="300"
             style={{margin: '2px'}}
             alt=""
             role="button"
           />
-        ))}
+        </div>
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          {props.images.map((src, index) => (
+            <img
+              src={ src }
+              onClick={() => changeImage(src, index)}
+              width="75"
+              key={ index }
+              style={{margin: '2px'}}
+              alt=""
+              role="button"
+            />
+          ))}
+        </div>
       </div>
       <div style={{float: 'left'}}><button className="me-2 btn btn-sm btn-outline-secondary w-100 mt-2" onClick={nextImage}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-right-fill" viewBox="0 0 16 16"><path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/></svg></button></div>
     </div>
